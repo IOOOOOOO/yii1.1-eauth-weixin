d8555fc78da367667d82be2a7eac375544f544d7
magic <ysjheeqg@163.com>
2016-03-03 16:58:32 +0800

    delete all

--- eauth/views/QRcode.php
+++
@@ -1,5 +0,0 @@
-<div id="login_container"></div>
-<script src="http://res.wx.qq.com/connect/zh_CN/htmledition/js/wxLogin.js"></script>
-<script type="text/javascript">
-var obj = new WxLogin(<?php echo $parameter ?>);
-</script>
